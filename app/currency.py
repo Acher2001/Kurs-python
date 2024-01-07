9942cf9ff59fc8f4004cd2d9b5c90472ab9f50ca
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2024-01-07 21:07:51 +0300

    add BaseCurrency class

--- app/currency.py
+++ app/currency.py
@@ -1,12 +1,23 @@
 import xml.etree.ElementTree as ET
 
-class Currency:
-    def __init__(self, data, code, amount=1, debug=False):
+
+class BaseCurrency:
+    def __init__(self, code, amount=1, debug=False, name=None):
         self.code = code
         self.debug = debug
         self.amount = amount
-        self.name = None
-        self.process_rates(data)
+        self.name = name
+        self.value = 1
+
+    def get_rel_rate(self, another):
+        rel_rate = round(another.value / self.value, 2)
+        return rel_rate, f'{self.code.lower()}-{another.code.lower()}: {rel_rate}'
+
+class Currency(BaseCurrency):
+    def __init__(self, data, code, amount=1, debug=False):
+        super().__init__(code=code, amount=amount, debug=debug)
+        self.process_rates(data=data)
+        
 
 
     def process_rates(self, data):
@@ -24,7 +35,3 @@ class Currency:
         self.value = float(cur.find('Value').text.replace(',', '.'))
         if self.debug:
             return currency.text
-
-    def get_rel_rate(self, another):
-        rel_rate = round(another.value / self.value, 2)
-        return rel_rate, f'{self.code.lower()}-{another.code.lower()}: {rel_rate}'
