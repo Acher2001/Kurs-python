a868ba94cec81d376d14f461ee15261df0951f2b
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2024-01-18 14:29:27 +0300

    init api

--- app/main.py
+++ app/main.py
@@ -2,8 +2,32 @@ import aiohttp
 import asyncio
 import logging
 import argparse
+from aiohttp import web
 
 from currency import Currency, BaseCurrency, display_currencies
+
+#----------API modules------------------------
+async def web_get_currency(request):
+    global valutes
+    for cur in valutes:
+        if cur.code.lower() == request.match_info.get('name'):
+            currency = cur
+            break
+    if cur.code == 'RUB':
+        return web.Response(body=f'rub: {cur.amount}')
+    else:
+        return web.Response(body=f'{cur.code.lower()}: {cur.amount}\n{valutes[0].get_rel_rate(cur)[1]}')
+
+async def init_app():
+    app = web.Application()
+    app.router.add_routes([
+        web.get('/{name}/get', web_get_currency)
+    ])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, 'localhost', 8080)
+    await site.start()
+#--------------------------------------------
 
 async def fetch_data(period, lock, url='https://www.cbr-xml-daily.ru/daily_utf8.xml'):
     """Получаем данные о курсе валют в формате xml"""
@@ -40,7 +64,7 @@ async def main():
     global valutes, args, data
     period = args.period
     data_lock = asyncio.Lock()
-    tasks = [fetch_data(period, data_lock), console(valutes, data_lock)]
+    tasks = [fetch_data(period, data_lock), console(valutes, data_lock), init_app()]
     await asyncio.gather(*tasks)
 
 
