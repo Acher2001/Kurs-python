6a808f2b05996e97328412ff47dc4babe709702d
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2024-01-31 14:07:55 +0300

    def inline kb for GET req

--- app/bot_features/keyboards.py
+++ app/bot_features/keyboards.py
@@ -1,6 +1,16 @@
 from aiogram.types import (KeyboardButton, Message, ReplyKeyboardMarkup,
-                           ReplyKeyboardRemove)
+                           ReplyKeyboardRemove, InlineKeyboardButton,
+                           InlineKeyboardMarkup)
 
 kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Текущий баланс')],
                                     [KeyboardButton(text='Текущий курс')],
                                     [KeyboardButton(text='Изменить баланс')]])
+
+def get_inline_kb_get(codes):
+    buttons = []
+    for code in codes:
+        buttons.append(InlineKeyboardButton(
+            text=code,
+            callback_data=f'get_{code.lower()}'
+        ))
+    return InlineKeyboardMarkup(inline_keyboard=[buttons])
